e78302d7fb752b08e5997c8d6099da4d0c69326b
rogeriofbi10 <rogeriofbigamer@gmail.com>
2026-02-09 19:28:40 -0300

    Create supabase.js

---
+++ supabase.js
@@ -0,0 +1,10 @@
+// supabase.js
+import { createClient } from "https://esm.sh/@supabase/supabase-js@2";
+
+export const SUPABASE_URL = "https://peoinxkkmeemlmgpugbv.supabase.co";
+export const SUPABASE_ANON_KEY = "sb_publishable_Wp8oZnHe8C9EdwJdV3satw_NF6KXdk4";
+
+export const supabase = createClient(
+  SUPABASE_URL,
+  SUPABASE_ANON_KEY
+);
